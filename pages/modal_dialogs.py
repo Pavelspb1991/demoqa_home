c984a132e9e14136138a23ce64b1cba0879ec6ca
Pavel <sn0oze@mail.ru>
2023-09-19 11:32:14 +0300

    First

--- pages/modal_dialogs.py
+++ pages/modal_dialogs.py
@@ -6,4 +6,4 @@ class Modal_dialogs(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.base_url = 'https://demoqa.com/modal-dialogs'
-        self.modal = WebElement(driver, "#item-")
+        self.modal = WebElement(driver, "div:nth-child(3) > div > ul > li")
